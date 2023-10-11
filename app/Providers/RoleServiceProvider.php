adacd0a14c4f05371333fcdc42efffb749fcd25a
Muhammad Ziyad Khan <ziyadkhan2333@gmail.com>
2023-10-11 19:07:44 +0500

    completed the roles crud with pages

---
+++ app/Providers/RoleServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\RoleRepository;
+use App\Services\RoleService;
+use Illuminate\Support\ServiceProvider;
+use Spatie\Permission\Models\Role;
+
+class RoleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(RoleRepository::class , function($app){
+            return new RoleRepository(new Role());
+        });
+        $this->app->bind(RoleService::class , function($app){
+            return new RoleService($app->make(RoleRepository::class));
+        });
+        $this->app->register(RoleInterfaceRepositoryServiceProvider::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
