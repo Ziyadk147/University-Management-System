d15ca6402f211837ac9086edf4c007a78b9b70ae
Muhammad Ziyad Khan <ziyadkhan2333@gmail.com>
2023-10-18 21:38:16 +0500

    user service repo and interface

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\UserInterface;
+use App\Models\User;
+
+class UserRepository implements UserInterface{
+
+    protected $user;
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+}
